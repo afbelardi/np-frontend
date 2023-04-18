99bf8103d0c6caa18a71861321e5287616c45a31
afbelardi <afbelardi@gmail.com>
2023-04-18 12:03:36 -0400

    navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import styles from '../styles/navbar.module.css';
+
+export default function Navbar () {
+    return (
+    <div className="flex justify-center w-full">
+    <div id={styles.navbar_custom} className="bg-base-100">
+  <div className="flex-1">
+    <a className="text-xl normal-case btn btn-ghost">NP Finder</a>
+  </div>
+  <div className="flex-none gap-2">
+    <div className="form-control">
+      <input type="text" placeholder="Search" className="input input-bordered" />
+    </div>
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </label>
+      <ul tabIndex={0} className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Navbar from '../components/Navbar';
 
 
 
@@ -6,6 +7,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
+      <Navbar />
     </>
   )
 }
