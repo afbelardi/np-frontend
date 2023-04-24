4e0f0aca3937176ba6774ef1ba56384bf5f9c09e
afbelardi <afbelardi@gmail.com>
2023-04-24 13:32:42 -0400

    skeleton coloring changed

--- src/pages/search.js
+++ src/pages/search.js
@@ -12,11 +12,11 @@ export default function Search () {
       return (
         <div
           role="status"
-          className="max-w-sm p-4 mb-5 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700"
+          className="max-w-sm p-4 mb-5 border rounded shadow border-slate-500 animate-pulse md:p-6"
         >
-          <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+          <div className="flex items-center justify-center h-48 mb-4 rounded bg-slate-500">
             <svg
-              className="w-12 h-12 text-gray-200 dark:text-gray-600"
+              className="w-12 h-12 text-gray-600"
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
               fill="currentColor"
@@ -25,13 +25,13 @@ export default function Search () {
               <pNameath d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
             </svg>
           </div>
-          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          <div className="h-2.5  rounded-full bg-slate-500 w-48 mb-4"></div>
+          <div className="h-2  rounded-full bg-slate-500 mb-2.5"></div>
+          <div className="h-2  rounded-full bg-slate-500 mb-2.5"></div>
+          <div className="h-2 bg-gray-700 rounded-full"></div>
           <div className="flex items-center mt-4 space-x-3">
             <svg
-              className="text-gray-200 w-14 h-14 dark:text-gray-700"
+              className="text-slate-500 w-14 h-14"
               aria-hidden="true"
               fill="currentColor"
               viewBox="0 0 20 20"
@@ -44,8 +44,8 @@ export default function Search () {
               ></path>
             </svg>
             <div>
-              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-              <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+              <div className="h-2.5 rounded-full bg-slate-500 w-32 mb-2"></div>
+              <div className="w-48 h-2 rounded-full bg-slate-500"></div>
             </div>
           </div>
           <span className="sr-only">Loading...</span>
@@ -55,40 +55,40 @@ export default function Search () {
 
     const Cards = () => {
       return (
-        <div class="max-w-sm rounded-lg shadow bg-gray-800 border-gray-700">
+        <div className="max-w-sm bg-gray-800 border-gray-700 rounded-lg shadow">
           <a href="#">
             <img
-              class="rounded-t-lg"
+              className="rounded-t-lg"
               src="/docs/images/blog/image-1.jpg"
               alt=""
             />
           </a>
-          <div class="p-5">
+          <div className="p-5">
             <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
                 Noteworthy technology acquisitions 2021
               </h5>
             </a>
-            <p class="mb-3 font-normal text-gray-400">
+            <p className="mb-3 font-normal text-gray-400">
               Here are the biggest enterprise technology acquisitions of 2021 so
               far, in reverse chronological order.
             </p>
             <a
               href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none  bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
             >
               Read more
               <svg
                 aria-hidden="true"
-                class="w-4 h-4 ml-2 -mr-1"
+                className="w-4 h-4 ml-2 -mr-1"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </a>
