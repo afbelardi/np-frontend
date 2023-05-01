d503687c8a544c9e6582cbdd3b20e297bc6cfcef
afbelardi <afbelardi@gmail.com>
2023-05-01 13:10:05 -0400

    moved useEffect to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Head from "next/head";
 import { AuthContext } from "../../authContext";
 import { useRouter } from "next/router";
+import jwt from 'jsonwebtoken';
 
 
 export default function Login () {
@@ -21,6 +22,30 @@ useEffect(() => {
         router.replace('/search')
     }
 })
+
+
+useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        setIsLoggedIn(true)
+
+        const decodedToken = jwt.decode(token);
+        const userId = decodedToken.userId;
+        (async () => {
+            try {
+                const response =  await axios.get(`http://localhost:8000/api/users/${userId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }   
+            })
+            const data = await response.data;
+            setUser(data);
+            } catch (error) {
+                console.error(error)
+            }
+        })()
+    }   
+}, [isLoggedIn]);
 
 const handleLogin = async (e) => {
     try {
