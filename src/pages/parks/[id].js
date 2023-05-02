d0962aa6921fe85dbf575a4e51b5466b0a63b8eb
afbelardi <afbelardi@gmail.com>
2023-05-02 16:09:42 -0400

    pulling in specific park data

--- src/pages/parks/[id].js
+++ src/pages/parks/[id].js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import Navbar from '../../components/Navbar';
 import { AuthContext } from '../../../authContext';
 import { useContext, useEffect } from 'react';
+import axios from 'axios';
 
 export default function ParkDetails ({ park }) {
 
@@ -15,7 +16,16 @@ export default function ParkDetails ({ park }) {
         if (!isLoggedIn) {
             router.replace('/login')
         }
-    })
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            const parkCode = id;
+            const response = await axios.get(`http://localhost:8000/api/nationalpark/apikey/park/${parkCode}`);
+            const data = await response.data.data[0];
+            console.log(data)
+        })()
+    }, [])
     
 
 
