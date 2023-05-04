8f8ba05bd38b50243898cbc1c8e48a8cefc6456c
afbelardi <afbelardi@gmail.com>
2023-05-03 20:30:57 -0400

    saving park data and image urls in localstorage

--- src/pages/parks/[id].js
+++ src/pages/parks/[id].js
@@ -4,7 +4,6 @@ import { AuthContext } from '../../../authContext';
 import { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import Header from '../../components/Header';
-import ImageCarousel from '../../components/ImageCarousel';
 import { Carousel } from 'flowbite-react';
 
 export default function ParkDetails ( ) {
@@ -14,22 +13,40 @@ export default function ParkDetails ( ) {
     const router = useRouter();
     const { id } = router.query;
     const [currentPark, setCurrentPark] = useState({})
+    const [parkImages, setParkImages] = useState([])
     
 
-    useEffect(() => {
-        if (!isLoggedIn) {
-            router.replace('/login')
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (!isLoggedIn) {
+    //         router.replace('/login')
+    //     }
+    // }, []);
 
     useEffect(() => {
         (async () => {
-            const parkCode = id;
-            const response = await axios.get(`http://localhost:8000/api/nationalpark/park/${parkCode}`);
-            const data = await response.data.data[0];
-            setCurrentPark(data);
+            try {
+                const parkCode = id;
+                const response = await axios.get(`http://localhost:8000/api/nationalpark/park/${parkCode}`);
+                const data = await response.data.data[0];
+                setCurrentPark(data);
+                setParkImages(data.images);
+                localStorage.setItem('currentPark', JSON.stringify(data));
+                localStorage.setItem('parkImages', JSON.stringify(data.images))
+            } catch(error) {
+                console.error(error)
+            }
+            
         })()
-    }, [])
+    }, [id]);
+
+    // useEffect(() => {
+    //     const storedPark = JSON.parse(localStorage.getItem('currentPark'));
+    //     const storedImages = JSON.parse(localStorage.getItem('parkImages'));
+    //     if (storedPark && storedImages) {
+    //         setCurrentPark(storedPark);
+    //         setParkImages(storedImages);
+    //     }
+    // }, [])
 
 
 
@@ -42,10 +59,10 @@ export default function ParkDetails ( ) {
             <Navbar />
             <h1 className="mt-10 ml-6 text-xl font-bold text-left text-white font-monserrat">{currentPark.fullName}</h1>
             <Carousel>
-               {currentPark.images.map(index => (
+               {parkImages.map(index => (
                     <img src={index.url} />
                 ))}
       </Carousel> 
         </>
     )
-}
+} 
