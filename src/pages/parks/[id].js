2e9d5086a7bef82786c381130c45f6d458fce335
afbelardi <afbelardi@gmail.com>
2023-05-04 12:13:18 -0400

    image carousel sized

--- src/pages/parks/[id].js
+++ src/pages/parks/[id].js
@@ -5,6 +5,8 @@ import { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import Header from '../../components/Header';
 import { Carousel } from 'flowbite-react';
+import styles from '../../styles/parkdetails.module.css';
+
 
 export default function ParkDetails ( ) {
 
@@ -30,23 +32,25 @@ export default function ParkDetails ( ) {
                 const data = await response.data.data[0];
                 setCurrentPark(data);
                 setParkImages(data.images);
-                localStorage.setItem('currentPark', JSON.stringify(data));
-                localStorage.setItem('parkImages', JSON.stringify(data.images))
+                console.log(data)
+                sessionStorage.setItem('currentPark', JSON.stringify(data));
+                sessionStorage.setItem('parkImages', JSON.stringify(data.images))
+                console.log('saved in session storage')
             } catch(error) {
                 console.error(error)
             }
             
         })()
-    }, [id]);
+    }, []);
 
     // useEffect(() => {
-    //     const storedPark = JSON.parse(localStorage.getItem('currentPark'));
-    //     const storedImages = JSON.parse(localStorage.getItem('parkImages'));
+    //         const storedPark = JSON.parse(sessionStorage.getItem('currentPark'));
+    //     const storedImages = JSON.parse(sessionStorage.getItem('parkImages'));
     //     if (storedPark && storedImages) {
     //         setCurrentPark(storedPark);
     //         setParkImages(storedImages);
-    //     }
-    // }, [])
+    //     }    
+    // }, [id]);
 
 
 
@@ -57,12 +61,19 @@ export default function ParkDetails ( ) {
         <>
             <Header />
             <Navbar />
-            <h1 className="mt-10 ml-6 text-xl font-bold text-left text-white font-monserrat">{currentPark.fullName}</h1>
-            <Carousel>
-               {parkImages.map(index => (
-                    <img src={index.url} />
-                ))}
-      </Carousel> 
+            <h1 className="mt-10 mb-10 ml-6 text-xl font-bold text-left text-white font-monserrat">{currentPark.fullName}</h1>
+            <div className="flex justify-center w-full h-1/2">
+            <section className={`${styles.carouselWrapper} flex justify-center w-full h-3/4`}>
+                <Carousel>
+                {parkImages.map(index => (
+                        <img 
+                        className="object-cover w-full h-full" 
+                        src={index.url} 
+                        />
+                    ))}
+                </Carousel> 
+            </section>
+            </div>
         </>
     )
 } 
