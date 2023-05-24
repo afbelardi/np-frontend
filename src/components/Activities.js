f486102ce2401bfe400d6ea964a064402fcd2a8b
afbelardi <afbelardi@gmail.com>
2023-05-24 15:34:48 -0400

    activities added

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -5,12 +5,21 @@ export default function Activities({ park }) {
     <div className="flex justify-center w-full">
     <div className='flex flex-col items-center w-11/12 mb-4 rounded-md bg-navbar-blue'>
         <section className="flex justify-center w-40 mt-2">
-            <FiActivity className="w-8 h-8 mr-2 text-white"/>
+            <FiActivity className="w-6 h-6 mt-1 mr-2 text-white"/>
             <h1 className="text-2xl font-bold text-white font-monserrat">Activities</h1>     
         </section>
-        <main className="flex flex-col items-center">
-            <p className="p-2 mb-2 font-semibold text-center text-white font-monserrat">{park.directionsInfo}</p>
-            <a className="mb-4 text-center text-white underline" target="_blank" href={park.directionsUrl}>Full Directions</a>
+        <main className="grid justify-center p-4">
+            <ul className="grid grid-cols-2 gap-1">
+                {park.activities.map((activity) => {
+                    return (
+                        <li className="flex items-center text-white font-monserrat">
+                            {activity.name}
+                        </li>
+                    )
+                   
+                }
+                   )}
+            </ul>
         </main>
     </div>
     </div>
