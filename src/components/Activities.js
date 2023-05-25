784ff35e7e59f7052cbf0ff97b451fcd9f6c12b4
afbelardi <afbelardi@gmail.com>
2023-05-25 14:56:37 -0400

    styled activities list

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -4,27 +4,24 @@ import { BsArrowReturnRight } from 'react-icons/bs';
 export default function Activities({ park }) {
   return (
     <div className="flex justify-center w-full">
-    <div className='flex flex-col items-center justify-center w-11/12 mb-4 rounded-md bg-navbar-blue'>
-        <section className="flex justify-center w-40 mt-2">
-            <FiActivity className="w-6 h-6 mt-1 mr-2 text-white"/>
-            <h1 className="text-2xl font-bold text-white font-monserrat">Activities</h1>     
-        </section>
-        <main className="flex justify-center w-full p-4">
-            <ul className="grid grid-cols-2 gap-1">
+        <div className='flex flex-col items-center justify-center w-11/12 mb-4 rounded-md bg-navbar-blue'>
+            <section className="flex justify-center w-40 mt-2">
+                <FiActivity className="w-6 h-6 mt-1 mr-2 text-white"/>
+                <h1 className="text-2xl font-bold text-white font-monserrat">Activities</h1>     
+            </section>
+            <ul className="flex flex-col items-center p-4">
                 {park.activities.map((activity) => {
                     return (
-                        <div className="flex items-center text-white">
+                        <>
+                        <li className="flex items-center font-semibold text-white font-monserrat">
                             <BsArrowReturnRight className="mr-2" />
-                            <li className="flex items-center justify-center text-white font-monserrat">
                                 {activity.name}
                             </li>
-                        </div>
-                    )
-                   
-                }
+                            </>
+                        )}
                    )}
             </ul>
-        </main>
+       
     </div>
     </div>
   )
