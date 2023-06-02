34f423cbed16cbb2873cf549d4338df54c2a630c
afbelardi <afbelardi@gmail.com>
2023-06-02 14:30:05 -0400

    headers added to cards

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -6,7 +6,7 @@ export default function Directions({ park }) {
   return (
     <div className="flex justify-center w-full mb-2">
     <div className={` ${styles.floatingDiv} flex flex-col items-center justify-center w-11/12 mb-4 rounded-md shadow-xl bg-navbar-blue`}>
-        <section className="flex justify-center w-40 mt-2">
+        <section className="flex items-center justify-center w-full h-16 bg-slate-800 rounded-tr-md rounded-tl-md">
             <TbMapSearch className="w-8 h-8 mr-2 text-white"/>
             <h1 className="text-2xl font-bold text-white font-monserrat">Directions</h1>     
         </section>
