ee8468073e2dba227a677f073bef718a960da1a2
afbelardi <afbelardi@gmail.com>
2023-07-14 14:43:53 -0400

    fixed table size and row spacing. Changed colors

--- src/components/FeesTable.js
+++ src/components/FeesTable.js
@@ -3,11 +3,11 @@
 export default function FeesTable({ park }) {
   return (
       <>
-        <div className="flex justify-center w-full mb-16">
-            <table className="w-3/4 text-sm text-left text-gray-400">
-                <thead className="text-xs text-gray-400 uppercase bg-navbar-blue">
+        <div className="flex justify-center w-full mb-16" >
+            <table className="w-3/4 text-sm text-gray-400">
+                <thead className="text-xs text-gray-400 uppercase border-separate rounded-lg bg-navbar-blue">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class=" px-6 py-3">
                             Title
                         </th>
                         <th scope="col" class="px-6 py-3">
@@ -18,17 +18,17 @@ export default function FeesTable({ park }) {
                         </th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className="">
                     {park.entranceFees.map(entranceFee => {
                         return (
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <tr className="bg-gray-800 border-b border-gray-700">
+                                <th scope="row" class="w-10 font-monserrat pl-6 font-semibold text-white">
                                     {entranceFee.title}
                                 </th>
-                                <td className="max-h-[100px] px-6 py-4">
+                                <td className="px-6 py-4 font-normal font-monserrat">
                                     {entranceFee.description}
                                 </td>
-                                <td className="px-6 py-4">
+                                <td className="px-6 py-4 font-monserrat">
                                     ${entranceFee.cost}
                                 </td>
                             </tr>
