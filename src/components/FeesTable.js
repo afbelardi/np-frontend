ecb14c99c7adb3a3e73e0d6d2f1b26e154ba8b75
afbelardi <afbelardi@gmail.com>
2023-07-13 21:07:44 -0400

    park information showing up in the table

--- src/components/FeesTable.js
+++ src/components/FeesTable.js
@@ -5,7 +5,7 @@ export default function FeesTable({ park }) {
       <>
         <div className="flex justify-center w-full mb-16">
             <table className="w-3/4 text-sm text-left text-gray-400">
-                <thead className="text-xs text-gray-400 uppercase bg-gray-700">
+                <thead className="text-xs text-gray-400 uppercase bg-navbar-blue">
                     <tr>
                         <th scope="col" class="px-6 py-3">
                             Title
@@ -19,17 +19,21 @@ export default function FeesTable({ park }) {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
-                        </th>
-                        <td className="px-6 py-4">
-                            Silver
-                        </td>
-                        <td className="px-6 py-4">
-                            Laptop
-                        </td>
-                    </tr>
+                    {park.entranceFees.map(entranceFee => {
+                        return (
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {entranceFee.title}
+                                </th>
+                                <td className="max-h-[100px] px-6 py-4">
+                                    {entranceFee.description}
+                                </td>
+                                <td className="px-6 py-4">
+                                    ${entranceFee.cost}
+                                </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </div>
