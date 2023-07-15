c1b9b363434968ec7000a7fe5b5bfda2ebdc51c8
afbelardi <afbelardi@gmail.com>
2023-07-15 18:23:42 -0400

    added button for show more and less

--- src/components/FeesTable.js
+++ src/components/FeesTable.js
@@ -1,9 +1,21 @@
-
+import { useState } from "react"
 
 export default function FeesTable({ park }) {
+
+
+    const [numResults, setNumResults] = useState(2);
+
+    const handleShowMore = () => {
+        setNumResults(numResults + 2);
+    }
+
+    const handleShowLess = () => {
+        setNumResults(2);
+    }
+
   return (
       <>
-        <div className="flex justify-center w-full mb-16" >
+        <div className="flex flex-col items-center justify-center w-full mb-16" >
             <table className="w-3/4 text-sm text-gray-400">
                 <thead className="text-xs text-gray-400 uppercase border-separate rounded-lg bg-navbar-blue">
                     <tr>
@@ -19,7 +31,7 @@ export default function FeesTable({ park }) {
                     </tr>
                 </thead>
                 <tbody className="">
-                    {park.entranceFees.map(entranceFee => {
+                    {park.entranceFees.slice(0, numResults).map(entranceFee => {
                         return (
                             <tr className="bg-gray-800 border-b border-gray-700">
                                 <th scope="row" class="w-10 font-monserrat pl-6 font-semibold text-white">
@@ -36,7 +48,24 @@ export default function FeesTable({ park }) {
                     })}
                 </tbody>
             </table>
+            {Object.keys(park.entranceFees).length > numResults && (
+          <button
+            className="px-6 py-2 mt-4 mb-4 text-sm font-medium text-gray-400 bg-gray-800 border border-gray-600 rounded-lg hover:text-white hover:bg-gray-700"
+            onClick={handleShowMore}
+          >
+            Show More
+          </button>
+        )}
+        {Object.keys(park.entranceFees).length <= numResults  && (
+          <button
+            className="px-6 py-2 mt-4 mb-4 text-sm font-medium text-gray-400 bg-gray-800 border border-gray-600 rounded-lg hover:text-white hover:bg-gray-700"
+            onClick={handleShowLess}
+          >
+            Show Less
+          </button>
+        )}
         </div>
+        
     </>
   )
 }
